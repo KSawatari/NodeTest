bb217229ba05afdafc30b12c60aeea83eae47d30
KSawatari <k.sawatari@micron-inc.jp>
2013-03-13 09:14:18 +0900

    commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var app = require('http').createServer( function (req, res) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) return res.writeHead(500);
+
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+var io = require('socket.io').listen(app, { log: false });
+io.sockets.on('connection', function (socket) {
+    socket.on('pulse', function (data) {
+        socket.emit('pulse', data ? data * 2 : 0);
+    });
+});
+
+app.listen(80);

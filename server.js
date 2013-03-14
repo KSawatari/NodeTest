f8e5929fe3dfe049762ffaa1aa8412ad4acf4537
KSawatari <k.sawatari@micron-inc.jp>
2013-03-14 09:00:39 +0900

    cmt

--- server.js
+++ server.js
@@ -13,6 +13,27 @@ io.sockets.on('connection', function (socket) {
     socket.on('pulse', function (data) {
         socket.emit('pulse', data ? data * 2 : 0);
     });
+	
+	/** 以下追加部分　http://d.hatena.ne.jp/ngmy/20111009/1318164221 **/
+  // クライアントからのイベント'all'を受信する
+	socket.on('all', function(data) {
+    // イベント名'msg'で受信メッセージを
+    // 自分を含む全クライアントにブロードキャストする
+		io.sockets.emit('msg', data);
+	});
+
+  // クライアントからのイベント'others'を受信する
+	socket.on('others', function(data) {
+    // イベント名'msg'で受信メッセージを
+    // 自分以外の全クライアントにブロードキャストする
+		socket.broadcast.emit('msg', data);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('disconn');
+	});
+	/** ここまで **/
+
 });
 
 app.listen(80);

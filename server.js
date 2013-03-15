788e9f6f1d4e6e452772fdc14130fed09383d799
KSawatari <k.sawatari@micron-inc.jp>
2013-03-15 10:07:51 +0900

    cm

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = require('http').createServer( function (req, res) {
 		fileName = persedURL.href;
 	}
 	
-    fs.readFile(__dirname + req.url, function (err, data) {
+    fs.readFile(__dirname +  '/index.html', function (err, data) {
         if (err) return res.writeHead(500);
         res.writeHead(200);
         res.end(data);

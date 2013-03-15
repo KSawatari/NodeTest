07afcd82d7f720ddd9f43e1cfb09e1fab67cccfa
KSawatari <k.sawatari@micron-inc.jp>
2013-03-15 10:14:52 +0900

    dvc

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ var app = require('http').createServer( function (req, res) {
 	// 参考
 	// http://nodejs.jp/nodejs.org_ja/api/http.html#http_http_createserver_requestlistener
 	var persedURL = require('url').parse( req.url, true );
-	var fileName;
-	if( persedURL.href.length == 0 ){
-		fileName = '/index.html';
-	}else{
-		fileName = persedURL.href;
-	}
-	
     fs.readFile(__dirname +  persedURL.href, function (err, data) {
         if (err) return res.writeHead(500);
         res.writeHead(200);
